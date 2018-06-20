a7cdf8abdbf44c46dcbd5e0fb79daae7c14b8c55
Egor Orlov <u1rtus@yandex.ru>
2018-06-20 23:24:11 +0400

    Adding some code

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,25 @@
+
+// Global vars
+var turn = 0;
+var a,b,c,d,e,f,g,h,i;
+a=b=c=d=e=f=g=h=i=null;
+var cell = [a, b, c, d, e, f, g, h, i];
+
 $(document).ready(function() {
-  
+
+  // ASSIGNMENT OF THE STAUS
+  $('.cell').click(function() {
+
+    // Check if cell is assigned and who's the turn 
+    if ( cell[this.id] === null && turn == 0) {
+
+    }
+    else if ( cell[this.id] === null && turn == 1) {
+
+    }
+    else {
+      alert("placed!")
+    }
+  });
+
 });

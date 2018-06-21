87e20e6d587b0ddca26d6bad1ebf72dee9216c8e
Egor Orlov <u1rtus@yandex.ru>
2018-06-21 11:49:59 +0400

    Added scale js libs

--- Scripts/main.js
+++ Scripts/main.js
@@ -10,16 +10,19 @@ $(document).ready(function() {
   // ASSIGNMENT OF THE STAUS
   $('.cell').click(function() {
 
-    // Check if cell is assigned and who's the turn 
+    // Check if cell is assigned and who's the turn
     if ( cell[this.id] === null && turn == 0) {
-
+      $(this).append("<div class='x'></div>");
+      turn = 1;
+      cell[this.id] = 0;
     }
     else if ( cell[this.id] === null && turn == 1) {
-
+      $(this).append("<div class='o'></div>");
+      turn = 0;
+      cell[this.id] = 0;
     }
     else {
-      alert("placed!")
+      $(this).children().animate({scale: 1.1}, 100).animate({scale: 1}, 100);
     }
   });
-
 });
